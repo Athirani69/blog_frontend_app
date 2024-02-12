89eb5b6a6537de606058c33df7fea8abc1193598
Athirani69 <Athirani69@gmail.com>
2024-02-12 10:58:19 +0530

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:blogapp/Pages/menu.dart';
 import 'package:flutter/material.dart';
 
+void main()
+{
+  runApp(blog());
+}
+
+class blog extends StatelessWidget {
+  const blog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: menu(),
+    );
+  }
+}
